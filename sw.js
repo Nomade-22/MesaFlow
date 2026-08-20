96acb3a6f8c94138139a6dbd03f829a09bf93564
Nomade-22 <jhonygui@gmail.com>
2026-08-20 10:26:26 -0300

    V7: limpar cache antigo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mesaflow-v6';
-const ASSETS=['./','./index.html','./styles.css?v=6','./js/config.js?v=6','./js/boot-v6.js?v=6','./v6.css.gz.b64?v=6','./js/app-v6.js.gz.b64?v=6','./manifest.webmanifest','./assets/grao-logo.svg'];
+const CACHE='mesaflow-v7';
+const ASSETS=['./','./index.html','./styles.css?v=7','./js/config.js?v=7','./js/boot-v6.js?v=7','./v6.css.gz.b64?v=7','./js/app-v6.js.gz.b64?v=7','./manifest.webmanifest','./assets/grao-logo.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();if(new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
