2a9c8cee41c42c56670da1d16dbe419505b4e34d
Nomade-22 <jhonygui@gmail.com>
2026-08-20 08:36:22 -0300

    Refresh PWA cache strategy

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE = "mesaflow-v1";
+const CACHE = "mesaflow-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./js/config.js", "./js/app.js", "./manifest.webmanifest"];
-self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+  event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
     return response;
-  }).catch(() => cached)));
+  }).catch(() => caches.match(event.request)));
 });
