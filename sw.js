faf2ca48a1ebda944b6ce087e95ae5636dafa709
Nomade-22 <jhonygui@gmail.com>
2026-08-20 10:57:27 -0300

    V10: atualizar cache do service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mesaflow-v8';
-const ASSETS=['./','./index.html','./styles.css?v=8','./js/config.js?v=8','./js/boot-v6.js?v=8','./manifest.webmanifest','./assets/grao-logo.svg'];
+const CACHE='mesaflow-v10';
+const ASSETS=['./','./index.html','./styles.css?v=10','./js/config.js?v=10','./js/boot-v6.js?v=10','./manifest.webmanifest','./assets/grao-logo.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();if(new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
